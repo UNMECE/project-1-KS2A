73d7fd0f429c9bf6ad5100f5b67adfbd2e1f6222
Kyan Slack <157538557+KS2A@users.noreply.github.com>
2024-10-14 13:48:58 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,11 @@
 #include <math.h>
 #include "capacitor.h"
 
-void voltage(double *time, double *voltage, double *current, double C) {
-
-
-
-}
-
-void current(double* time, double* voltage, double* current, double C) {
-
-
-
-}
-
 int main() {
 
 	Capacitor capm;
 
+	// assigning values
 	double dt = 1e-10;
 	double final_time = 5e-6;
 	int timestep_no = 50000;
@@ -28,12 +17,28 @@ int main() {
 	double V_0 = 10.0;
 	double num_output = 200;
 
-	double* time = (double*)malloc(sizeof(double));
-	double* voltage = (double*)malloc(sizeof(double));
-	double* current = (double*)malloc(sizeof(double));
+	// allocating memory for capacitor struct arrays
+	capm.time = (double*)malloc(timestep_no * sizeof(double));
+	capm.voltage = (double*)malloc(timestep_no * sizeof(double));
+	capm.current = (double*)malloc(timestep_no * sizeof(double));
 
-	for (int i = 0; i < timestep_no; i++) {
+	capm.voltage[0] = 0.0;
+	capm.current[0] = V_0 / R;
 
+	// printing current and voltage for every 200 timestamps
+	for (int i = 1; i < timestep_no; i++) {
+		capm.time[i] = i * dt;
+		// work out current at time t
+		capm.current[i] = capm.current[i - 1] - (capm.current[i - 1] / (R * C)) * dt;
+		// work out voltage at time t
+		capm.voltage[i] = capm.voltage[i - 1] + capm.current[i - 1] * dt / C;
+
+		if (i % 200 == 0) {
+			printf("Time = %e\n", capm.time[i]);
+			printf("Current = %e\n", capm.current[i]);
+			printf("Voltage = %e\n", capm.voltage[i]);
+			printf("\n");
+		}
 	}
 
 	return 0;
